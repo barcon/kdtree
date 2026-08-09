123ee53b778d942a7ae7c91bb251bd1b4567d98f
barcon <12259909+barcon@users.noreply.github.com>
2026-08-09 08:06:51 +0200

    Updated build system

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,35 @@
 -- premake5.lua
 workspace "kdtree"
-	configurations { "Debug", "Release" }
+	configurations { "Debug", "Release", "ReleaseCL"  }
 	location "build"
 
 project "kdtree"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
+	architecture "x86_64"
+	objdir "%{cfg.location}/obj/%{cfg.platform}_%{cfg.buildcfg}"
 
 	targetdir "build/%{cfg.buildcfg}"
 	includedirs { "../utils/src" }	
 	includedirs { "../logger/src" }	
 	includedirs { "../eilig/src" }	
 	includedirs { "../basis/src" }	
-	includedirs { "../node/src" }	
+	includedirs { "../node/src" }		
 
 	files { "src/**.hpp", "src/**.cpp" }
 
-	filter "configurations:Debug"
-		architecture "x86_64"     
+	filter "configurations:Debug"  
 		defines { "DEBUG" }
 		symbols "On"
 
 	filter "configurations:Release"
-		architecture "x86_64"     
 		defines { "NDEBUG" }
 		optimize "Speed"
+
+	filter "configurations:ReleaseCL"   
+		defines { "NDEBUG", "EILIG_ENABLE_OPENCL" }
+		optimize "Speed"	
+		
+		includedirs { "../club/src" }	
+		includedirs { "../opencl/inc" }
